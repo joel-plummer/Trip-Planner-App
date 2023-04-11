c51574848c805578264ca30df2a3e70d3f690aa3
Shamar Malcolm <70509256+marko-a87@users.noreply.github.com>
2023-04-10 21:38:15 -0500

    Add files via upload

---
+++ Trip-Planner-App-mine-/Proj3/Account.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Account {
+    /*
+     * Attributes that should be here:
+     * String Username
+     * String Password
+     * Double Budget
+     * Account/App Settings (maintaining chosen theme for example)
+     * Trip obj
+     */
+
+    // calcremaining needs to be adjusted
+    // we may end up changing this file a bit
+
+    private String username;
+    private String password;
+    private double budget;
+    private double remaining;
+    private String theme;
+    private ArrayList<Trip> myTrips;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        budget = 0.0;
+        remaining = 0.0;
+        theme = "Default";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public double getRemaining() {
+        return remaining;
+    }
+
+    public void setUsername(String newUser) {
+        username = newUser;
+    }
+
+    public void setPassword(String newPass) {
+        password = newPass;
+    }
+
+    public void setBudget(double newBudget) {
+        budget = newBudget;
+    }
+
+    public void setTheme(String newTheme) {
+        theme = newTheme;
+    }
+
+    public void calcRemaining(double cost) {
+        // remaining = budget - cost;
+    }
+
+    public String toString() {
+        return (getUsername() + "\t" + getPassword() + "\t" + getBudget());
+    }
+
+    public String toFile() {
+        return getUsername() + "" + getPassword();
+    }
+
+}
