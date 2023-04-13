32eaaa291212932852e368b131370b6deb4b6667
flywithkye <129135109+flywithkye@users.noreply.github.com>
2023-04-13 16:51:16 -0600

    Update Bus.java

--- Proj3/Bus.java
+++ Proj3/Bus.java
@@ -1,14 +1,21 @@
 
-
 public class Bus {
     /* 
         Attributes that should be here:
-        Enum Type: Small, Medium, Large
+        Enum Type: Small, Medium, Luxurious
         Int Auto Generated ID (format: B10)
         Int maxCapacity
     */
 
+    BusType type; //There is another file for this
 
+    public Bus(BusType type) {
+        this.type = type;
+    }
+
+    public BusType getType(){
+        return type;
+    }
 
 
 }
